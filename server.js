1624b4d731a25d9dbe05c0cb2fcadeae67dad8dc
ilkinsufi <ilkinsufi@gmail.com>
2024-02-10 17:23:40 +0400

    Routes add

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", {text: "world"});
+  res.render("index", { text: "world" });
 });
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 
 app.listen(3000);
